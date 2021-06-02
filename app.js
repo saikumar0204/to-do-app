3f04ad7571e0fb4ffd01d28cdc724e941885ddbe
saikumar0204 <44923451+saikumar0204@users.noreply.github.com>
2021-06-02 14:17:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express=require('express')
 var todocontroller= require('./controllers/todocontroller')
 var app= express()
-
+var port= process.env.PORT||3000
 
 app.set('view engine','ejs')
 app.use(express.static('./public'))
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 
-app.listen(3000)
+app.listen(port)
 console.log('Server is running on port 3000');
 
 //fire controller
